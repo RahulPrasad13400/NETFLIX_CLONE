10b481f039f7bafc4adc417c16a775c36250333d
RahulPrasad13400 <rahulprasad1399@gmail.com>
2025-02-23 13:13:51 +0530

    home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,21 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
+import Footer from "./components/Footer";
 
 export default function App() {
-  return <Routes>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/login" element={<LoginPage />} />
-    <Route path="/signup" element={<SignUpPage />} />
-    <Route path="/watch/:id" element={<HomePage />} />
-    <Route path="/search" element={<HomePage />} />
-    <Route path="/history" element={<HomePage />} />
-    <Route path="*" element={<HomePage />} />
-  </Routes>
+  return <>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/watch/:id" element={<HomePage />} />
+      <Route path="/search" element={<HomePage />} />
+      <Route path="/history" element={<HomePage />} />
+      <Route path="*" element={<HomePage />} />
+    </Routes>
+    <Footer />
+  </>
+
 }
 
