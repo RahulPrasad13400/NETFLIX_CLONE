feb5f31a77457998e6d3e9d19436d99c05f3500d
RahulPrasad13400 <rahulprasad1399@gmail.com>
2025-03-04 07:05:50 +0530

    completed

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cookieParser from "cookie-parser"
+import path from "path"
 
 import authRoutes from "./routes/auth.route.js"
 import movieRoutes from "./routes/movie.route.js"
@@ -12,6 +13,8 @@ import { protectRoute } from "./middleware/protectRoute.js"
 
 const app = express()
 const PORT = ENV_VARS.PORT || 5000
+const __dirname = path.resolve()
+
 app.use(express.json()) // this is the fn that allow us to use req.body 
 app.use(cookieParser()) // this allow to access the cookies 
 
@@ -19,6 +22,13 @@ app.use('/api/v1/auth', authRoutes)
 app.use('/api/v1/movie', protectRoute, movieRoutes)
 app.use('/api/v1/tv', protectRoute, tvRoutes)
 app.use('/api/v1/search', protectRoute, searchRoutes)
+
+if(process.env.NODE_ENV.trim() === "production"){
+        app.use(express.static(path.join(__dirname, "/frontend/dist")))
+        app.get("*", (req, res)=>{
+            res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+        })
+}
 
 app.listen(PORT,()=>{
     console.log("server running..", PORT)
